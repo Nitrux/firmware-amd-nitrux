e728ff1baee8ceffce505f03e542b7ca56a8df2f
UriHerrera <hello@uriherrera.com>
2022-01-15 07:00:43 -0600

    chk

--- travis-build.sh
+++ travis-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 apt -qq update
-apt -qq -yy install equivs curl git
+apt -qq -yy install equivs git --no-install-recommends
 
 ### Install Dependencies
-apt -qq -yy install devscripts lintian build-essential automake autotools-dev
+apt -qq -yy install devscripts lintian --no-install-recommends
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
