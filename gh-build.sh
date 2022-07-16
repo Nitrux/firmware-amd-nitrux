9a4f374cfe5fb515d2e40fb2b96fc12225a28bba
Uri Herrera <hello@uriherrera.com>
2022-07-16 00:07:13 -0500

    [ci skip]

--- gh-build.sh
+++ gh-build.sh
@@ -11,6 +11,6 @@ mk-build-deps -i -t "apt-get --yes" -r
 ### Build Deb
 debuild -b -uc -us
 
+### Move Deb to current directory because debuild decided
+### that it was a GREAT IDEA TO PUT THE FILE ONE LEVEL ABOVE
 mv ../*.deb . 
-
-pwd; ls -l
